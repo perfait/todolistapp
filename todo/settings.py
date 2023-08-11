8b8788edf21a3f2fc853a10a1b36ad87dd1bee4f
perfait <pafeshisoks@gmail.com>
2023-08-11 23:35:50 +0300

    staticfiles

--- todo/settings.py
+++ todo/settings.py
@@ -121,7 +121,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
