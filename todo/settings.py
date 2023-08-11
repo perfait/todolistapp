c52fd4cf2d288fb5a08e57d49a98629e4824c82c
perfait <pafeshisoks@gmail.com>
2023-08-11 23:41:55 +0300

    changing postgres connection details

--- todo/settings.py
+++ todo/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'todolist',
         'USER': 'akaka',
-        'PASSWORD': 'puffnane1738',
+        'PASSWORD': 'puffnane',
         'HOST': 'localhost',
         'PORT': '',
     }
