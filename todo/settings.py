7cbc83a492e7b6213a965af7dafc542ccb8fa7df
perfait <pafeshisoks@gmail.com>
2023-08-13 21:53:41 +0300

    updates db settings

--- todo/settings.py
+++ todo/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'todolist',
         'USER': 'akaka',
-        'PASSWORD': 'puffnane',
+        'PASSWORD': 'puffnane1738',
         'HOST': 'localhost',
         'PORT': '',
     }
